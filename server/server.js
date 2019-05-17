4ea889c9ade66677e6b8dab95e2e589a7a7c7b98
Bobby Khounphinith <khoupe07@gmail.com>
2019-05-17 17:16:47 -0500

    Created GET route to get all images from database.

--- server/server.js
+++ server/server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
+const imageRouter = require('./routes/image.router');
+// const tagRouter = require('./routes/tag.router');
+// const imgTagRouter = require('./routes/imgtag.router');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.use('/api/image', imageRouter);
+// app.use('/api/tag', tagRouter);
+// app.use('/api/image_tag', imgTagRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
